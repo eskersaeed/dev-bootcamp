7c6aebdcce86a2cb28f9f939fc6eb52adec002f9
Saeed <saeedmahmoud2@gmail.com>
2026-05-18 23:10:28 +0100

    Master if, else if, else, comparison operators, and logical operators

---
+++ ChapterThree/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ChapterThree;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("--- Logical Operators ---");
+
+bool isEmployed = true;
+bool hasGoodCredit = false;
+
+// AND (&&): BOTH conditions must be true
+bool loanApproved = isEmployed && hasGoodCredit;
+Console.WriteLine("Loan Approved (AND): " + loanApproved);
+
+// OR (||): AT LEAST ONE condition must be true
+bool provisionalApproval = isEmployed || hasGoodCredit;
+Console.WriteLine("Provisional Approval (OR): " + provisionalApproval);
+
+// NOT (!): Negates the boolean value
+bool isUnemployed = !isEmployed;
+Console.WriteLine("Is Unemployed (NOT): " + isUnemployed); 
+    }
+}
